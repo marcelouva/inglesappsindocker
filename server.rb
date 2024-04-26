cbc181ea45bde01c067170ba1d22c774a4ee3e83
Marcelo <uva@dc.exa.unrc.edu.ar>
2024-04-26 18:35:05 -0300

    se incorporan unas lineas al inicio de server.rb para conf. la base de datos - se actualiza el leeme

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require 'yaml'
+require_relative 'models/user'
 
+set :database, { adapter: "sqlite3", database: "dbingles.sqlite3" }
 
   get '/' do
     'Welcome'
